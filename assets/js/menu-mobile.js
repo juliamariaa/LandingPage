89dc0de0246b39b301916e95fbd5cadbebac172e
juliamariaa <juliamariadev@gmail.com>
2023-07-06 23:17:39 -0300

    corrigindo erro do menu mobile

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -1,13 +1,13 @@
 // Função para modificar ícone do menu mobile
 const menuMobileShow = () => {
     let menu = document.querySelector('.nav-menuMobileLink')
-    let menuIcon = document.querySelector("menu-icon");
+    let menuIcon = document.querySelector(".menu-icon");
     
     if (menu.style.display === "block") {
         menu.style.display = "none";
         menuIcon.src = "assets/img/icons-menu.png";
     } else {
         menu.style.display = "block";
-        menuIcon.src = "assets/img/icons-menuClosed";
+        menuIcon.src = "assets/img/icons-menuClosed.png";
     }
 }
